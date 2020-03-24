aa1eb2ccca2ff7cdb23820ba4a22d4eaaaa99303
JamesAnthonyDellaMorte <jamesanthony@delresearch.com>
2020-03-24 16:26:50 -0400

    added comments and some test text files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This program will take an input text and find all the unique words and tell you how many times they appear in a text. Thus telling you many words are needed to understand all the vocabulary in a text.  
+This program will take an input text and find all the unique words 
+and tell you how many times they appear in a text. 
+Thus telling you many words are needed to understand all the vocabulary in a text.
+
+It is run like so ./historust path/to/file
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use std::fs::File;
 extern crate regex;
 use regex::Regex;
 use counter::Counter;
+/*
+This function will remove all unnecessary chars from the input file i.e  punctuation, digits and some spanish chars
+It will also turn the input text all lowercase
 
+*/
 fn stripfile(mut input_file: File) ->  std::string::String
 {   let mut contents = String::new();
     let strip_misc = Regex::new(r"[[:punct:][:digit:][¿¡]]").unwrap();
@@ -18,20 +22,20 @@ fn main() {
 
     let input_path = ::std::env::args().nth(1).unwrap(); // Getting input file
 
-    let v =  File::open(&input_path).unwrap();
+    let input_file =  File::open(&input_path).unwrap();
 
-    let mut p = File::create("output.txt").expect("Error");
-    let contents = stripfile(v);
-    let words = &contents.split_whitespace().collect::<Counter<_>>().most_common();
+    let mut output_file = File::create("output.txt").expect("Could not create output file");
+    let contents = stripfile(input_file);
+    let words = &contents.split_whitespace().collect::<Counter<_>>().most_common(); // Organizing data into a Counter collection
 
     for ele in words
         {
-            write!(p, "{:?}\n" ,ele).expect("Error");
+            write!(output_file, "{:?}\n" ,ele).expect("Could not write to file");
         }
     let unique_words = words.len();
     let total_words = contents.split_whitespace().count();
-    write!(p, "Number of unique words: {}\n" ,unique_words).expect("Error");
-    write!(p, "Number of total words: {}\n" ,total_words).expect("Error");
+    write!(output_file, "Number of unique words: {}\n" ,unique_words).expect("Error");
+    write!(output_file, "Number of total words: {}\n" ,total_words).expect("Error");
     println!("output.txt created");
     //  write!(p, "Percent of words needed to know to understand 100% of text: {}" ,(unique_words as f64).div(total_words as f64)).expect("Error");
 }
